ad5e6240f703df72e5803fabbb65b35e4b3797e2
jorgeanciel <jorgeanciel@hotmail.com>
2022-07-22 22:44:58 -0500

    empezando

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import PageMain from "../page/PageMain";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../components/Header"
+const Router=() =>{
+    return(
+        <BrowserRouter>
+         <Header/>
+         
+         <Routes>
+           <Route path="/" element={<PageMain/>}/>
+         </Routes>
+         
+         
+        </BrowserRouter>
+    )
+}
+export default Router;
